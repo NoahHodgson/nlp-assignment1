653826af5ed44b945b345424e7624d8c0f401dde
Noah <noahmhodgson@gmail.com>
2022-02-20 14:40:31 -0500

    this is the playground

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
 def main():
-    print("Hello World!")
+    first_file = open("test1.txt", "r")
+    second_file = open("test2.txt", "r")
+    test1 = first_file.read()
+    test2 = second_file.read()
+    first_file.close()
+    second_file.close()
+    print("Done!")
 
 if __name__ == "__main__":
     main()
