3d694e57ebd8e84ab16ce6542319e929706186b5
tmfish13 <tmfish@udel.edu>
2022-02-27 12:35:29 -0500

    added def for question_cat and Q_type enum

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
+import re
+from enum import Enum
 
-def question_cat(question: str) -> str:
+class Q_type(Enum):
+    DID = 0
+    WHAT = 1
+    HOW = 2
+    INVALID = 3
+
+
+def question_cat(question: str) -> Q_type:
+    if(re.search("\ADid", question) != None):
+        return Q_type.DID
+    if(re.search("\AWhat", question) != None):
+        return Q_type.WHAT
+    if(re.search("\AHow", question) != None):
+        return Q_type.HOW
+    return Q_type.INVALID
 
 
 def inc_or_dec(question: str) -> bool:
-
+    return
 
 def find_line(company: str, inc_or_dec: bool) -> str:
-
+    return None
 
 def find_amt(company: str, line: str) -> int:
-
-
+    return
 
 def main():
     first_file = open("test1.txt", "r")
